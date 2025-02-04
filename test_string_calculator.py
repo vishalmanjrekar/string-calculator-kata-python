b68da974e0649046cb21d24c38add4846cf27601
Vishal Manjrekar <manjrekarv48@gmail.com>
2025-02-04 14:00:13 +0530

    Test: newline as a delimiter along with commas

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -18,6 +18,10 @@ class TestStringCalculator(unittest.TestCase):
     def test_multiple_numbers_comma_delimited(self):
         self.assertEqual(self.calculator.add("1,2,3,4"), 10)
 
+    def test_newline_delimiter(self):
+        self.assertEqual(self.calculator.add("1\n2,3"), 6)
+
+
 
 if __name__ == "__main__":
     unittest.main()
