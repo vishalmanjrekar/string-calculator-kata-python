7ed5a1e2300c84d1d17e8e8b225e693923349cbc
Vishal Manjrekar <manjrekarv48@gmail.com>
2025-02-04 13:05:28 +0530

    Test: Ensure empty string returns 0.

---
+++ test_string_calculator.py
@@ -0,0 +1,14 @@
+import unittest
+from string_calculator import StringCalculator
+
+
+class TestStringCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calculator = StringCalculator()
+
+    def test_empty_string_returns_zero(self):
+        self.assertEqual(self.calculator.add(""), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
