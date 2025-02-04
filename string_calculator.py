688e908c4a71ab9f5a1ccf108f4707849dc648e2
Vishal Manjrekar <manjrekarv48@gmail.com>
2025-02-04 14:09:04 +0530

    Imp: support newlines as delimiters along with commas

--- string_calculator.py
+++ string_calculator.py
@@ -1,6 +1,8 @@
+import re
+
 class StringCalculator:
     def add(self, numbers):
         if not numbers:
             return 0
-        num_list = numbers.split(",")
+        num_list = re.split(r",|\n", numbers)  # Split by comma or newline
         return sum(int(num) for num in num_list)
