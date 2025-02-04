7451a84f5c9d0bd01151d69c35b04716ab97a331
Vishal Manjrekar <manjrekarv48@gmail.com>
2025-02-04 14:27:23 +0530

    Imp: support for custom delimiters

--- string_calculator.py
+++ string_calculator.py
@@ -4,5 +4,12 @@ class StringCalculator:
     def add(self, numbers):
         if not numbers:
             return 0
-        num_list = re.split(r",|\n", numbers)  # Split by comma or newline
+        
+        delimiter = ",|\n"  # Default delimiters
+        
+        if numbers.startswith("//"):
+            delimiter, numbers = numbers[2:].split("\n", 1)  # Extract custom delimiter
+            delimiter = re.escape(delimiter)  # Escape special characters for regex
+        
+        num_list = re.split(delimiter, numbers)
         return sum(int(num) for num in num_list)
